af7f99ea9760132b04246d1adffe78ce0b950dc9
nancyobrien <nancy@stepframe.com>
2017-12-02 11:47:31 -0800

    configure with settings from standard boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,72 @@
+'use strict';
+const webpack = require('webpack');
 const path = require('path');
- 
+const loaders = require('./webpack.loaders');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const DashboardkPlugin = require('webpack-dashboard/plugin');
+
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || '8080';
+
+loaders.push({
+	test: /\.scss$/,
+	loaders: ['style.loader', 'css-loader?importLoaders=1', 'sass-loader'],
+	exclude: ['node_modules']
+})
+
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js',
-  ],
-  output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-    ],
-  },
-  resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+	entry: [
+		'babel-polyfill',
+		'./src/index.js',
+  	],
+  	devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+  	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js',
+  	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		//Allow locating dependancies relative to the src directory and then the js directory
+		//avoid .. hell
+		modules: [
+			path.join(__dirname, '/src'),
+			path.join(__dirname, '/src/js'),
+			'node_modules'
+		],
+		alias {
+			symbol: 'es6-symbol',
+			'~': path.join(__dirname, '/src')
+		}
+	},
+	module: {
+		loaders
+	},
+	devServer: {
+		contentBase: './public',
+		//do not print bundle build state
+		noInfo: true,
+		//embed the webpack-dev-server runtime into the bundle
+		inline: true,
+		//serve index.html in place of 404 responses to allow HTML5 history
+		historyAPIFallback: true,
+		port: PORT,
+		host: HOST
+	},
+	plugins: [
+		new webpack.NoEmitOnErrorsPlugin(),
+		new webpack.NamedModulesPlugin(),
+		new ExtractTextPlugin({
+			filename: 'style.css',
+			allChunks: true
+		}),
+		new DashboadPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/template.html',
+			files: {
+				css: ['style.css'],
+				js: ['bundle.js']
+			}
+		})
+	]
 };
